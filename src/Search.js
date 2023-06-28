5828ec75f8c3107bc6e2b0440eed1c91946c326f
YaroslavKazeyev <yaroslav.kazeyev@gmail.com>
2023-06-28 20:06:00 +0200

    Go back to the initial state

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,33 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 
-function Search(props){
-    const [searchInput, setSearchInput] = useState("");
-    function changeHandler(event){
-        setSearchInput(event.target.value)
+class Search extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            searchInput: ""
+        }
     }
-    return(
-        <form onSubmit={(event) => props.formSubmited(event, searchInput)}>
-            <input 
-                onChange={changeHandler} 
-                type="text" 
-                placeholder="Cafe is by default, search yours" 
-                name="searchInput" 
-                id="searchInput"
-            />
-            <input type="submit" value="Search" onSubmit={(event) => props.formSubmited(event, searchInput)} />
-        </form>
-    )
+
+    changeHandler=(event)=>{
+        this.setState({searchInput: event.target.value})
+    }
+
+
+    render() {
+        return (
+            <div>
+                 <input 
+                 onChange={this.changeHandler} 
+                 type="text" 
+                 placeholder="Cafe is by default, search yours" 
+                 name="searchInput" 
+                 id="searchInput"
+                />
+                <button onClick={this.props.getVenues.bind(null, this.state.searchInput)}>Search</button>
+            </div>
+        )
+    }
 }
+
 export default Search;
