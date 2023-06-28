88422d74a0bb544d8e1d175600fd5def7f5aad0a
YaroslavKazeyev <yaroslav.kazeyev@gmail.com>
2023-06-28 19:08:07 +0200

    Search is a functional component

--- src/Search.js
+++ src/Search.js
@@ -1,33 +1,21 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class Search extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            searchInput: ""
-        }
+function Search(props){
+    const [searchInput, setSearchInput] = useState("");
+    function changeHandler(event){
+        setSearchInput(event.target.value)
     }
-
-    changeHandler=(event)=>{
-        this.setState({searchInput: event.target.value})
-    }
-
-
-    render() {
-        return (
-            <div>
-                 <input 
-                 onChange={this.changeHandler} 
-                 type="text" 
-                 placeholder="Cafe is by default, search yours" 
-                 name="searchInput" 
-                 id="searchInput"
-                />
-                <button onClick={this.props.getVenues.bind(null, this.state.searchInput)}>Search</button>
-            </div>
-        )
-    }
+    return(
+        <form onSubmit={(event) => props.formSubmited(event, searchInput)}>
+            <input 
+                onChange={changeHandler} 
+                type="text" 
+                placeholder="Cafe is by default, search yours" 
+                name="searchInput" 
+                id="searchInput"
+            />
+            <input type="submit" value="Search" onSubmit={(event) => props.formSubmited(event, searchInput)} />
+        </form>
+    )
 }
-
 export default Search;
